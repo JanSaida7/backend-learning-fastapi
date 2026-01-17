f50d99ba10e31d18ec160cc8bb7d721bef34938f
Jan <jansaidasyed743@gmail.com>
2026-01-18 01:33:35 +0530

    Day3: Added SavingsAccount subclass

--- Day3/banking_system.py
+++ Day3/banking_system.py
@@ -3,9 +3,9 @@ import datetime
 
 class BankAccount:
     def __init__(self, owner: str, balance: float = 0.0):
-        self.owner = owner
-        self.balance = balance
-        self.transaction_history = []
+        self.owner: str = owner
+        self.balance: float = balance
+        self.transaction_history: list[str] = []
 
     def _log_transaction(self, description: str):
         # Helper method to format time and add to history
@@ -29,3 +29,16 @@ class BankAccount:
         for transaction in self.transaction_history:
             print(transaction)
         print(f"Final Balance: ${self.balance}")
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self, owner: str, balance: float = 0.0, interest_rate: float = 0.05):
+        # Call the parent constructor
+        super().__init__(owner, balance)
+        self.interest_rate: float = interest_rate
+
+    def apply_interest(self) -> None:
+        interest: float = self.balance * self.interest_rate
+        self.deposit(interest)  # Reuse the parent's deposit logic!
+        self._log_transaction(f"Interest applied (${interest})")
+        print(f"Interest applied: ${interest}")   
