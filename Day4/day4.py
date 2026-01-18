01449c35393a58a359b0dea5609b7236b1526077
Jan <jansaidasyed743@gmail.com>
2026-01-19 00:11:15 +0530

    Day 4: Implemented manual function wrapping

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,5 +1,13 @@
 import time
 
+# 1. The Wrapper Factory
+def my_logger(func):  # Changed from func_to_decorate
+    def wrapper():
+        print("LOG: Starting process...")
+        func()  # Updated reference
+        print("LOG: Process finished.")
+    return wrapper
+
 def get_user_data():
     print("Fetching data from database...")
     time.sleep(1.5)
@@ -7,4 +15,10 @@ def get_user_data():
 
 if __name__ == "__main__":
     print("--- APP STARTED ---")
-    get_user_data()
+    
+    # 2. The Manual Decoration
+    # We wrap the function manually
+    secure_function = my_logger(get_user_data)
+
+    # 3. Run the NEW function
+    secure_function()
