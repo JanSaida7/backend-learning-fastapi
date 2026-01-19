8405054d3734bdd56edd3baa845ae9eacbd3586a
Jan <jansaidasyed743@gmail.com>
2026-01-19 23:38:02 +0530

    Day4: Added execution_timer decorator

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,5 +1,5 @@
 import time
-from typing import Callable
+from typing import Callable, Any
 
 def my_logger(func_to_decorate: Callable[[], None]) -> Callable[[], None]:
     def wrapper():
@@ -15,8 +15,30 @@ def get_user_data():
     time.sleep(1.5)
     print("Data fetched!")
 
+
+#The Timer Decorator
+def execution_timer(func: Callable[..., Any]) -> Callable[..., Any]:
+    # *args, **kwargs means "Accept ANY arguments"
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"⏱️ {func.__name__} took {end - start:.4f} seconds")
+        return result
+    return wrapper
+
+@execution_timer
+def heavy_calculation(n: int) -> int:
+    print(f"Calculating sum of range {n}...")
+    time.sleep(1)
+    return sum(range(n))
+
 if __name__ == "__main__":
     print("--- APP STARTS ---")
 
-
     get_user_data()
+
+
+    print("\n--- TIMER TEST ---")
+    total = heavy_calculation(1000000)
+    print(f"Result: {total}")
